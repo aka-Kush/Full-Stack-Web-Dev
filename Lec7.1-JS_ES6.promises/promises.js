e8c4ee2246e4c1810eb4cd4ae0fc13853466a8d4
Shivansh Kush <kushivansh27@gmail.com>
2020-11-24 02:12:02 +0530

    Update

---
+++ Lec7.1-JS_ES6.promises/promises.js
@@ -0,0 +1,13 @@
+/*Async operations like promises are put into an event queue, which runs after the main thread has finished processing so 
+that they do not block subsequent JavaScript code from running. The queued operations will complete as soon as possible then return their 
+results to the JavaScript environment. */
+
+//Difference between Asynchronous and Synchronous
+/*
+* Synchronous basically means that you can only execute one thing at a time. 
+* Asynchronous means that you can execute multiple things at a time and you don't have to finish executing the current thing in order to move on to next one 
+*/
+
+
+
+
