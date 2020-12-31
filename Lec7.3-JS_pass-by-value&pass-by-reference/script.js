7c224e4413aeace2373dc213da4759c03b2083b5
aka-Kush <kushivansh27@gmail.com>
2020-12-31 20:29:14 +0530

    Update

---
+++ Lec7.3-JS_pass-by-value&pass-by-reference/script.js
@@ -0,0 +1,6 @@
+//Pass by Value  &  Pass by Reference
+/**
+ ** Pass by Value:  When we pass a value to a function, a copy of the value is made inside the function and any changes made won't be reflected to the Original value
+ ** Pass by Reference:  We pass the pointer to the array which points to the original variables, and any changes made will be reflected to the original variables
+ */
+
